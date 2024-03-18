63953c4695ebae16153a7261c026403e118736cf
DUSERNAM3 <117265173+DUSERNAM3@users.noreply.github.com>
2024-03-19 00:34:28 +0300

    v 1.1

--- PATTERN BLYAT/PATTERN BLYAT/Program.cs
+++ PATTERN BLYAT/PATTERN BLYAT/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -50,34 +52,56 @@ class RPGStats
         return sb.ToString();
     }
 }
-class Program
-{
-    static void Main(string[] args)
-    {
-        // содаем объект создателя
-        RPGCreate Creator = new RPGCreate();
-        // создаем билдер для класса Человек
-        RPGBuilder builder = new Human();
-        // билдим Человек
-        RPGStats Human = Creator.Build(builder);
-        Console.WriteLine(Human.ToString());
-        // создаем билдер для класса Нежить
-        builder = new Undead();
-        // билдим Нежить
-        RPGStats Undead = Creator.Build(builder);
-        Console.WriteLine(Undead.ToString());
-        // создаем билдер для класса Нежить
-        builder = new Elf();
-        // билдим Нежить
-        RPGStats Elf = Creator.Build(builder);
-        Console.WriteLine(Elf.ToString());
 
-        Console.Read();
+class Menu
+{
+    public int choice;
+    public void MenuFunc()
+    {
+        while (true)
+        {
+            Console.WriteLine("    Меню:\n" +
+            "1) Создать персонажа класса Человек\n" +
+            "2) Создать персонажа класса Нежить\n" +
+            "3) Создать персонажа класса Эльф\n" +
+            "4) Выход\n" +
+            "Выберите желаемое действие: \n");
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            // создаем объект создателя и билдера
+            RPGCreate Creator = new RPGCreate();
+            RPGBuilder builder;
+            switch (choice)
+            {
+                case 1:
+                    builder = new Human(); 
+                    RPGStats Human = Creator.Build(builder);
+                    Console.WriteLine(Human.ToString());
+                    break;
+                case 2: 
+                    builder = new Undead();
+                    RPGStats Undead = Creator.Build(builder);
+                    Console.WriteLine(Undead.ToString());
+                    break;
+                case 3:
+                    builder = new Elf();
+                    RPGStats Elf = Creator.Build(builder);
+                    Console.WriteLine(Elf.ToString());
+                    break;
+                case 4: return;
+                default:
+                    Console.WriteLine("Введена неправильная команда.\n Попробуйте снова");
+                    break;
+            }
+        }
+  
+        
     }
 }
 // абстрактный класс строителя
 abstract class RPGBuilder
 {
+    public string readname;
     public RPGStats RPGModel { get; private set; }
     public void CreateModel()
     {
@@ -101,7 +125,7 @@ class RPGCreate
         return RolePlay.RPGModel;
     }
 }
-// строитель для ржаного хлеба
+// строитель для класса Человек
 class Human : RPGBuilder
 {
     public override void SetHP()
@@ -118,10 +142,18 @@ class Human : RPGBuilder
     }
     public override void SetName()
     {
-        this.RPGModel.Name = new Names { name = "Player1" };
+        Console.Write("Введите имя для расы Человек: ");
+        while (true)
+        {
+            readname = Console.ReadLine();
+            Console.Clear();
+            if (readname.Length > 12) Console.Write("Введенное имя превышает максимальную длину.\nПопробуйте снова.\n");
+            else break;
+        }
+        this.RPGModel.Name = new Names { name = readname };
     }
 }
-// строитель для пшеничного хлеба
+// строитель для класса Нежить
 class Undead : RPGBuilder
 {
     public override void SetHP()
@@ -138,9 +170,18 @@ class Undead : RPGBuilder
     }
     public override void SetName()
     {
-        this.RPGModel.Name = new Names { name = "Player2" };
+        Console.Write("Введите имя для расы Нежить: ");
+        while (true)
+        {
+            readname = Console.ReadLine();
+            Console.Clear();
+            if (readname.Length > 12) Console.Write("Введенное имя превышает максимальную длину.\nПопробуйте снова.\n");
+            else break;
+        }
+        this.RPGModel.Name = new Names { name = readname };
     }
 }
+//Строитель для класса Эльф
 class Elf : RPGBuilder
 {
     public override void SetHP()
@@ -157,6 +198,31 @@ class Elf : RPGBuilder
     }
     public override void SetName()
     {
-        this.RPGModel.Name = new Names { name = "Player3" };
+        Console.Write("Введите имя для расы Эльф: ");
+        while (true) {
+            readname = Console.ReadLine();
+            Console.Clear();
+            if (readname.Length > 12) Console.Write("Введенное имя превышает максимальную длину.\nПопробуйте снова.\n");
+            else break;
+        }
+        this.RPGModel.Name = new Names { name = readname };
+    }
+}
+
+class None : RPGBuilder
+{
+    public override void SetHP(){}
+    public override void SetMP(){}
+    public override void SetClassName(){}
+    public override void SetName(){}
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.MenuFunc();
+        Console.Read();
     }
 }
